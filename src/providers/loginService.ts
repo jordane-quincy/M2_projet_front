9830b0da3104af0842a402a9edfb9a9da465ad63
morganbasset <morgan.basset@etu.univ-valenciennes.fr>
2017-06-06 14:22:42 +0200

    create notification page

--- src/providers/loginService.ts
+++ src/providers/loginService.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { environment } from '../constants/constants';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
@@ -14,11 +15,13 @@ import 'rxjs/add/operator/catch';
 @Injectable()
 export class LoginService {
 
+
   constructor(public http: Http) {
 
   }
 
   connect(param: Object) : Observable<any> {
+     let url: string = environment.backendUrl;
       const headers = new Headers();
         headers.append('Content-Type', 'application/json');
 
