874cbd41ca7db0e311ef75d27abb76f78c309894
morganbasset <morgan.basset@etu.univ-valenciennes.fr>
2017-06-06 16:09:29 +0200

    fix app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { LoginPage } from '../pages/login/login';
 import { OffersListPage } from '../pages/offers-list/offers-list';
 import { TabsPage } from '../pages/tabs/tabs';
-import { Login } from '../pages/login/login'
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = Login;
+  rootPage:any = LoginPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
