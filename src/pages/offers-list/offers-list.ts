17eba08515a4ff70044bfafc1eb506adef3a543b
axel.latour <axel.latour@cgi.com>
2017-06-06 13:11:19 +0200

    WIP OffersListPage

--- src/pages/offers-list/offers-list.ts
+++ src/pages/offers-list/offers-list.ts
@@ -1,22 +1,32 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { Offer } from '../../models/index';
 
-@IonicPage()
 @Component({
   selector: 'page-offers-list',
   templateUrl: 'offers-list.html',
 })
 export class OffersListPage {
 
-  offersList: Offer[];
+  offersList: any[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-
+    this.offersList = [{
+      title: 'title',
+      description: 'description'
+    },
+    {
+      title: "autre title",
+      description: 'autre description'
+    }];
   }
 
   ionViewDidLoad() {
 
+  }
+
+  handleOfferClick(): void {
+    console.log('click');
   }
 
 }
