09cd969f2ebf79c88c30973d0c06a8c1d465788a
Kaolyne <lemaire.pauline08@gmail.com>
2017-06-07 11:00:34 +0200

    add showConfirm()

--- src/pages/user-offers/user-offers.ts
+++ src/pages/user-offers/user-offers.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 
 import { AddStudentPage } from '../add-student/add-student';
 
@@ -11,11 +11,34 @@ export class UserOffersPage {
 
   userOffersList: any[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
     this.userOffersList = [];
+
   }
 
   ionViewDidLoad() {
+  }
+
+  showConfirm(){
+    let confirm = this.alertCtrl.create({
+      title: 'Confirmation de suppression',
+      message: 'Etes vous sur de vouloir supprimer cette offre ?',
+      buttons: [
+        {
+          text: 'Annuler',
+          handler:() => {
+            console.log('annulation');
+          }
+        },
+        {
+          text:'Supprimer',
+          handler: () => {
+            console.log('Suppression');
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 
   addStudent(){
