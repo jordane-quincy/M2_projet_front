47a916579c4fec36c2c9afbf96af2c6e72669786
JbRezidu <duriez.jeanbaptiste@gmail.com>
2017-06-07 10:51:35 +0200

    Finish link and composant autocomplete

--- src/pages/create-account-skills/create-account-skills.ts
+++ src/pages/create-account-skills/create-account-skills.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { CreateAccountService } from '../../providers/create-account-service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'page-create-account-skills',
@@ -8,11 +9,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CreateAccountSkillsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    console.log(navParams.get('body'));
+  user: any;
+
+  selectedSkills: Object[];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private createAccountService: CreateAccountService) {
+    this.user = _.cloneDeep(navParams.get('user'));
+    this.selectedSkills = [];
   }
 
   ionViewDidLoad() {
+  }
+
+  createAccount() {
+    this.user.skills = _.cloneDeep(this.selectedSkills);
+    (this.user.skills || []).map(skill => {
+      delete(skill.customSkill);
+    });
+    this.createAccountService.createAccount(this.user).subscribe(
+      res => {
+        console.log("success");
+      },
+      err => {
+        console.log("err");
+      }
+    );
   }
 
 }
