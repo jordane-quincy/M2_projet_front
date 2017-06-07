5404e6128d546072f8869cb4eb0485688423e5c5
clement1402 <lgkg850>
2017-06-07 12:35:43 +0200

    Stars

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -6,21 +6,70 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'profile.html',
 })
 export class ProfilePage {
-  profil: any;
+  private profile: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.profil = [{
+    // Initialisation du profil
+    this.profile = {
       picture: "",
       firstname: "Clément",
       lastname: "DELPECH",
       birthdate: "14/02/1993",
       etudiant: true,
       title: "M1 TNSI",
-      mark: 4
-    }]
+      mark: [
+        0,
+        0,
+        0,
+        0,
+        0
+      ]
+    }
+
+    // Définition des étoiles
+    this.starsDefinition(3);
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilPage');
+  starsDefinition(mark: any): void {
+    var i: any;
+    i = 0;
+
+    while(mark > 0)
+    {
+      if((mark - 1) >= 0)
+      {
+        mark = mark - 1;
+        this.profile.mark[i] = 1;
+      }
+      else if(mark - 0.5 >= 0)
+      {
+        mark = mark - 0.5;
+        this.profile.mark[i] = 0.5;
+      }
+      else if(mark >= 0.25 && mark < 0.5)
+      {
+        if(this.profile.mark[i-1] == 0.5)
+        {
+          mark = 0;
+          this.profile.mark[i-1] = 1;
+        }
+        else
+        {
+          mark = 0;
+          this.profile.mark[i] = 0.5;
+        }
+      }
+      else
+      {
+        mark = 0;
+      }
+
+      i++;
+    }
+  }
+
+  addCompetences(){
+    // this.navCtrl.push();
+    console.log("Je clique sur ajouter des compétences");
   }
 }
