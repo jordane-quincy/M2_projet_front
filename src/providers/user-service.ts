2697f2328de060d3977f0b9627611f50f0adebf1
JbRezidu <duriez.jeanbaptiste@gmail.com>
2017-06-08 11:17:05 +0200

    Fin mot de passe oublié

--- src/providers/user-service.ts
+++ src/providers/user-service.ts
@@ -35,13 +35,15 @@ export class UserService {
 
   // Ask if we can go to reset password screen, check if user exists with the email
   askForResetPassword(body: any): Observable<any> {
-    return this.http.post("http://httpbin.org/post", JSON.stringify(body))
+    let httpAddress: string = `${environment.backendUrl}/user/askResetPassword`
+    return this.http.post(httpAddress, JSON.stringify(body))
     .map(res => res.json())
     .catch((error: any) => Observable.throw(error.json() || 'Server error'));
   }
 
   resetPassword(body: any): Observable<any> {
-    return this.http.post("http://httpbin.org/post", JSON.stringify(body))
+    let httpAddress: string = `${environment.backendUrl}/user/resetPassword`
+    return this.http.post(httpAddress, JSON.stringify(body))
     .map(res => res.json())
     .catch((error: any) => Observable.throw(error.json() || 'Server error'));
   }
