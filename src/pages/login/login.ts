9ca74f12f0599d79ee69dd3edf61acf62ce96a83
JbRezidu <duriez.jeanbaptiste@gmail.com>
2017-06-08 16:43:33 +0200

    Link mon profil avec le back sauf avis

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavController, NavParams } from 'ionic-angular';
-import { AuthService, ToastService, TokenService, LoaderService } from '../../providers/index';
+import { AuthService, ToastService, TokenService, LoaderService, UserService } from '../../providers/index';
 import { CreateAccountPage } from '../create-account/create-account';
 import { ForgottenPasswordPage } from '../forgotten-password/forgotten-password';
 import { TabsPage } from '../tabs/tabs';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'page-login',
@@ -16,7 +17,7 @@ export class LoginPage {
 
   form: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, fb: FormBuilder, private authService: AuthService, private toastService: ToastService, private tokenService: TokenService, private loaderService: LoaderService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, fb: FormBuilder, private authService: AuthService, private toastService: ToastService, private tokenService: TokenService, private loaderService: LoaderService, private userService: UserService) {
     this.form = fb.group({
       email: fb.control('', [Validators.required, Validators.email, Validators.pattern(".*@(univ-valenciennes.fr|etu.univ-valenciennes.fr)")]),
       password: fb.control('', [Validators.required])
@@ -29,6 +30,7 @@ export class LoginPage {
       result => {
         this.tokenService.setToken(result.token);
         this.loaderService.dismissLoader();
+        this.userService.setUser(_.cloneDeep(result.user));
         this.navCtrl.push(TabsPage);
       },
       error => {
